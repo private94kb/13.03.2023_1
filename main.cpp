a24da563c33aed80eb9db8152a59c7753819fa0c
private94kb <91588734+private94kb@users.noreply.github.com>
2023-06-12 00:40:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+bool isLeap(int year);
+
+int main(void) {
+for(int yr = 1990; yr < 2023; yr++)
+cout << yr << " -> " << isLeap(yr) << endl;
+return 0;
+}
+
+bool isLeap(int year) {
+ if(year % 4 == 0){
+   return true;
+ }
+  else return false;
+}
+  
